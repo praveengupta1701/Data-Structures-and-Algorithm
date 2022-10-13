11957eb0f95d813c14f8e00313789462a4cc01d6
praveengupta1701 <praveengupta.17jan@gmail.com>
2022-10-14 00:09:31 +0530

    Update queueusingsingle_stack.cpp

--- Stack and Queue/queueusingsingle_stack.cpp
+++ Stack and Queue/queueusingsingle_stack.cpp
@@ -1,30 +1,4 @@
-Last chance to avail FLAT (35% + 20%) Off on all Coding Ninjas Courses| Enrol Now
-Skip to content
-takeUforward
-~ Strive for Excellence
-
-Striver’s A2Z DSA Course/Sheet
-Striver’s SDE Sheet
-Striver’s DP Series
-CS Subjects
-SDE Core Sheet
-Operating Systems
-Computer Networks
-Programming
-Learn C++
-C++ STL
-C Programs
-Data Structure
-Interview Prep
-Blind 75
-Striver Tree Series
-Striver’s CP Sheet
-TCS NQT Coding Sheet
-Interview Experience
-Interview Prep Roadmap
-Courses
-December 7, 2021 Data Structure / Queue / Stack
-Implement Queue using Stack
+/*
 Problem Statement: Given a Stack having some elements stored in it. Can you implement a
 Queue using the given Stack?
 
@@ -55,7 +29,7 @@ size()->
 size() operation is for returning the size of a queue which can be done by using the function Stack1. size(). It will actually return the total number of elements in the queue.
 
 Code:
-
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -102,6 +76,37 @@ struct Queue {
     return input.size();
   }
 };
+
+class MyQueue {
+public:
+    stack <int> input,output;
+     void push(int x) {
+        input.push(x);
+    }
+  // Remove the element from the front of the queue and returns it...
+    int pop() {
+        peek();
+       int val = output.top();
+        output.pop();
+        return val;
+    }
+  // Get the front element...
+    int peek() {
+        if (output.empty())
+        while (!input.empty())
+            output.push(input.top()), input.pop();
+        return output.top();
+    }
+  // Return whether the queue is empty...
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+    int size() {
+    return (output.size() + input.size()); 
+  }
+
+};
+  
 int main() {
   Queue q;
   q.Push(3);
