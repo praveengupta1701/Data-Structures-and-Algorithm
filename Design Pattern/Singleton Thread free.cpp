cb8324e557551c97a6741ffca7023c5e66771c84
praveengupta1701 <praveengupta.17jan@gmail.com>
2022-10-13 20:58:37 +0530

    added

--- Design Pattern/Singleton Thread free.cpp
+++ Design Pattern/Singleton Thread free.cpp
@@ -5,9 +5,18 @@
 #include <mutex>
 using namespace std;
 
-
+/**
+ * The Singleton class defines the `GetInstance` method that serves as an
+ * alternative to constructor and lets clients access the same instance of this
+ * class over and over.
+ */
  class Singleton
  {
+    /**
+     * The Singleton's constructor/destructor should always be private to
+     * prevent direct construction/desctruction calls with the `new`/`delete`
+     * operator.
+     */
      private:
          static Singleton * _instance;
          static std::mutex mutex_;
@@ -28,21 +37,26 @@ using namespace std;
           * Singletons should not be assignable.
           */
          void operator=(const Singleton &) = delete;
+         /**
+         * This is the static method that controls the access to the singleton
+         * instance. On the first run, it creates a singleton object and places it
+         * into the static field. On subsequent runs, it returns the client existing
+         * object stored in the static field.
+         */
 
          //static Singleton *GetInstance(const std::string& value);
-         static Singleton *GetInstance(const std::string& value)
-         {
-             if (_instance == nullptr)
-             {
-                 std::lock_guard<std::mutex> lock(mutex_);
-                 if (_instance == nullptr)
-                 {
-                     _instance = new Singleton(value);
-                 }
-             }
-             return _instance;
-         }
 
+         static Singleton *GetInstance(const std::string& value);
+      
+
+          /**
+         * Finally, any singleton should define some business logic, which can be
+         * executed on its instance.
+         */
+        void SomeBusinessLogic()
+        {
+            // ...
+        }
          std::string value() const{
              return value_;
          }
@@ -55,13 +69,34 @@ using namespace std;
  std::mutex Singleton::mutex_;
 
 
+ /**
+ * The first time we call GetInstance we will lock the storage location
+ *      and then we make sure again that the variable is null and then we
+ *      set the value. RU:
+ */
+Singleton *Singleton::GetInstance(const std::string& value)
+{
+     if (_instance == nullptr)
+     {
+         std::lock_guard<std::mutex> lock(mutex_);
+         if (_instance == nullptr)
+         {
+             _instance = new Singleton(value);
+         }
+     }
+     return _instance;
+ }
+
+
  void ThreadFoo(){
+     // Following code emulates slow initialization.
      std::this_thread::sleep_for(std::chrono::milliseconds(10));
      Singleton* singleton = Singleton::GetInstance("FOO");
      std::cout << singleton->value() << "\n";
  }
 
  void ThreadBar(){
+      // Following code emulates slow initialization.
      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
      Singleton* singleton = Singleton::GetInstance("BAR");
      std::cout << singleton->value() << "\n";
